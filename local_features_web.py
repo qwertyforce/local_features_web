188c02b943b0f3f5b1eadb5f89d49335f10a3ff8
qwertyforce <qwertyforce@protonmail.com>
2022-08-28 17:16:41 +0300

    add sparse keypoints

--- local_features_web.py
+++ local_features_web.py
@@ -7,7 +7,7 @@ from typing import Optional, Union
 import cv2
 import torch
 import numpy as np
-import math
+from math import sqrt
 import kornia as K
 import asyncio
 import kornia.feature as KF
@@ -20,17 +20,25 @@ import psycopg2
 from kornia_moons import feature
 from PIL import Image
 import io 
+from numba import jit
+from numba.core import types
+from numba.typed import Dict
 
 dim = 128
 index = None
 DATA_CHANGED_SINCE_LAST_SAVE = False
 laf_from_opencv_SIFT_kpts = feature.laf_from_opencv_SIFT_kpts
-detector = cv2.SIFT_create(nfeatures=200)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 HardNet8 = KF.HardNet8(True).eval().to(device)
 LAST_POINT_ID = 1
+FIND_SPARSE_KEYPOINTS = True
+N_KEYPOINTS = 200
 #FIND_MIRRORED = True
 app = FastAPI()
+if FIND_SPARSE_KEYPOINTS == True:
+    detector = cv2.SIFT_create(contrastThreshold=-1)
+else:
+    detector = cv2.SIFT_create(nfeatures=N_KEYPOINTS)
 
 def main():
     global app, DB_img_points, DB_keypoints, DB_descriptors, LAST_POINT_ID
@@ -51,6 +59,94 @@ def main():
     loop = asyncio.get_event_loop()
     loop.call_later(10, periodically_save_index,loop)
 
+@jit(nopython=True, cache=True, fastmath=True)
+def check_distance(target_keypoint_x, target_keypoint_y, keypoints, keypoints_neighbors):
+    skip_flag = False
+    for kpt in keypoints:
+        if kpt[0] == 0 and kpt[1] == 0: #_keypoints is zeroed
+            break
+        x,y = kpt
+        dist = sqrt((target_keypoint_y-y)**2 + (target_keypoint_x-x)**2)
+        if dist < 50: #is_neighbor
+            hash = ((x + y)*(x + y + 1)/2) + y # https://stackoverflow.com/a/682617
+            if keypoints_neighbors[hash] >= 3:
+                skip_flag = True
+                break
+    return skip_flag
+
+@jit(nopython=True, cache=True, fastmath=True)
+def update_neighbors(target_keypoint_x, target_keypoint_y, keypoints, keypoints_neighbors):
+    new_kpt_hash = ((target_keypoint_x + target_keypoint_y)*(target_keypoint_x + target_keypoint_y + 1)/2) + target_keypoint_y
+    keypoints_neighbors[new_kpt_hash]=0
+    for kpt in keypoints:
+        x,y = kpt
+        if x == target_keypoint_x and y == target_keypoint_y:
+            continue
+        if x == 0 and y == 0: #_keypoints is zeroed
+            break
+        dist = sqrt((target_keypoint_y-y)**2 + (target_keypoint_x-x)**2)
+        if dist < 50: #is_neighbor
+            hash = ((x + y)*(x + y + 1)/2) + y # https://stackoverflow.com/a/682617
+            keypoints_neighbors[hash]+=1
+
+def get_keypoints_sparse(img,n_features=200):
+    height= img.shape[0]
+    width= img.shape[1]
+    height_divided_by_2 = img.shape[0]//2
+    width_divided_by_2 = img.shape[1]//2
+    _keypoints = np.zeros((n_features, 2)) #_keypoints is only used to pass it to numba optimized functions, because they can't use keypoints(it's list of cv2.Keypoint)
+    kps = detector.detect(img,None)
+    kps = sorted(kps, key = lambda x:x.response,reverse=True)
+    keypoints_count = [0,0,0,0]
+    keypoints=[]
+    N = n_features//4
+    used_keypoints = 0
+    keypoints_neighbors = Dict.empty(key_type=types.float64, value_type=types.int64)
+    def add_kpt(kpt):
+        nonlocal used_keypoints, _keypoints, keypoints
+        x,y = kpt.pt
+        _keypoints[used_keypoints][0] = x
+        _keypoints[used_keypoints][1] = y
+        keypoints.append(kpt)
+        update_neighbors(x, y, _keypoints, keypoints_neighbors)
+        used_keypoints+=1
+
+    for keypoint in kps:
+        keypoint_x,keypoint_y=keypoint.pt
+        if len(keypoints) != 0:
+            skip_keypoint = check_distance(keypoint_x, keypoint_y, _keypoints, keypoints_neighbors)
+            if skip_keypoint:
+                continue
+
+        if used_keypoints == 200:
+            break
+
+        if keypoints_count[0]<N and 0<keypoint_y<height_divided_by_2 and 0<keypoint_x<width_divided_by_2:
+            add_kpt(keypoint)
+            keypoints_count[0]+=1
+            continue
+
+        if keypoints_count[1]<N and 0<keypoint_y<height_divided_by_2 and width_divided_by_2<keypoint_x<width:
+            add_kpt(keypoint)
+            keypoints_count[1]+=1
+            continue
+
+        if keypoints_count[2]<N and height_divided_by_2<keypoint_y<height and 0<keypoint_x<width_divided_by_2:
+            add_kpt(keypoint)
+            keypoints_count[2]+=1
+            continue
+
+        if keypoints_count[3]<N and height_divided_by_2<keypoint_y<height and 0<width_divided_by_2<keypoint_x<width:
+            add_kpt(keypoint)
+            keypoints_count[3]+=1
+            continue
+    return keypoints
+
+def get_keypoints(img):
+    if FIND_SPARSE_KEYPOINTS:
+        return get_keypoints_sparse(img, N_KEYPOINTS)
+    return detector.detect(img, None)
+
 def int_from_bytes(xbytes: bytes) -> int:
     return int.from_bytes(xbytes, 'big')
 
@@ -70,6 +166,7 @@ def prepare_db():
     cur.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = 'ambience'")
     exists = cur.fetchone()
     if not exists:
+        conn.commit()
         conn.autocommit = True
         cur.execute('CREATE DATABASE ambience')
         conn = psycopg2.connect(connect_settings_ambience)
@@ -155,7 +252,7 @@ def resize_img_to_threshold(img):
     width, height = img.size
     threshold = 3000*3000
     if height*width > threshold:
-        k = math.sqrt(height*width/threshold)
+        k = sqrt(height*width/threshold)
         img = img.resize((round(width/k), round(height/k)),Image.Resampling.LANCZOS)
     return img
 
@@ -182,7 +279,7 @@ def get_features(image_buffer, mirrored=False):
     img = np.array(img)
     if mirrored:
         img = np.fliplr(img)
-    kpts = detector.detect(img, None)
+    kpts = get_keypoints(img)
     if len(kpts) == 0:
         return None
     with torch.no_grad():
---
+++ remove_last_images.py
@@ -0,0 +1,80 @@
+import psycopg2
+import lmdb
+from tqdm import tqdm
+
+def create_table(conn):
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS img_points (
+            image_id integer NOT NULL UNIQUE,
+            point_id_range int4range NOT NULL)""")
+    cur.execute('CREATE INDEX IF NOT EXISTS point_id_range_gist_index ON img_points USING GIST (point_id_range);')
+    conn.commit()
+
+def prepare_db():
+    connect_settings_postgres = "dbname=postgres host=localhost user=postgres password=12345"
+    connect_settings_ambience = "dbname=ambience host=localhost user=postgres password=12345"
+    conn = psycopg2.connect(connect_settings_postgres)
+    cur = conn.cursor()
+    cur.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = 'ambience'")
+    exists = cur.fetchone()
+    if not exists:
+        conn.autocommit = True
+        cur.execute('CREATE DATABASE ambience')
+        conn = psycopg2.connect(connect_settings_ambience)
+        create_table(conn)
+        return conn
+    else:
+        conn = psycopg2.connect(connect_settings_ambience)
+        create_table(conn)
+        return conn
+
+def delete_img_points(image_id):
+    cursor = DB_img_points.cursor()
+    cursor.execute("DELETE FROM img_points WHERE image_id = %s",[image_id])
+    DB_img_points.commit()
+
+def delete_descriptors(point_ids):
+    with DB_descriptors.begin(write=True, buffers=True) as txn:
+        for point_id in point_ids:
+            txn.delete(point_id)
+
+def delete_keypoints(point_ids):
+    with DB_keypoints.begin(write=True, buffers=True) as txn:
+        for point_id in point_ids:
+            txn.delete(point_id)
+
+
+DB_img_points = prepare_db()
+DB_keypoints = lmdb.open('./keypoints.lmdb',map_size=10 * 1000 * 1_000_000) #6gb
+DB_descriptors = lmdb.open('./descriptors.lmdb',map_size=120 * 1000 * 1_000_000) #120gb
+
+def get_point_ids(image_id):
+    cursor = DB_img_points.cursor()
+    cursor.execute("SELECT point_id_range FROM img_points WHERE image_id = %s",[image_id])
+    result = cursor.fetchone()
+    if result is None:
+        return []
+    else:
+        return list(range(result[0].lower,result[0].upper))
+
+def get_last_inserted_image():
+    cursor = DB_img_points.cursor()
+    cursor.execute("SELECT MAX(lower(point_id_range)) FROM img_points")
+    result = cursor.fetchone()
+    last_image_id = result[0]
+    cursor.execute("SELECT image_id FROM (SELECT image_id,MAX(lower(point_id_range)) as mmax FROM img_points GROUP BY image_id )t1 WHERE mmax = %s",[last_image_id])
+    result = cursor.fetchone()
+    return result[0]
+
+def int_to_bytes(x: int) -> bytes:
+    return x.to_bytes((x.bit_length() + 7) // 8, 'big')
+
+# print(get_last_inserted_image())
+
+for i in tqdm(range(11000)):
+    image_id = get_last_inserted_image()
+    point_ids = get_point_ids(image_id)
+    point_ids_bytes = [int_to_bytes(x) for x in point_ids]
+    delete_img_points(image_id)
+    delete_keypoints(point_ids_bytes)
+    delete_descriptors(point_ids_bytes)
